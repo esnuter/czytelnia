5133463e189c03141e2a6c0e3f53d5d732188108
esnuter <wielgus.kuba2005@gmail.com>
2025-04-14 15:18:53 +0200

    Add user authentication: login, register, logout with Flask-Login and Flask-Bcrypt

---
+++ app.py
@@ -0,0 +1,88 @@
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from flask_bcrypt import Bcrypt
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, ValidationError
+from extensions import db, login_manager, bcrypt
+from models import User
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'tajny-klucz-123'  # 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///czytelnia.db'
+
+db.init_app(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Register Form
+class RegisterForm(FlaskForm):
+    username = StringField('Nazwa użytkownika', validators=[DataRequired(), Length(min=3, max=50)])
+    password = PasswordField('Hasło', validators=[DataRequired(), Length(min=6)])
+    submit = SubmitField('Zarejestruj się')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Nazwa użytkownika zajęta!')
+
+# Login Form
+class LoginForm(FlaskForm):
+    username = StringField('Nazwa użytkownika', validators=[DataRequired()])
+    password = PasswordField('Hasło', validators=[DataRequired()])
+    submit = SubmitField('Zaloguj się')
+
+# Routes
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Konto stworzone! Możesz się zalogować.', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect(url_for('home'))  #
+        else:
+            flash('Błędna nazwa użytkownika lub hasło!', 'danger')
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/')
+def home():
+    return "Witaj w Czytelnia.pl!"  #
+
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
+    if not User.query.filter_by(username='admin').first():
+        hashed_password = bcrypt.generate_password_hash('admin123').decode('utf-8')
+        admin = User(username='admin', password=hashed_password, is_moderator=True)
+        db.session.add(admin)
+        db.session.commit()
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- models.py
+++ models.py
@@ -1,12 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-
-db = SQLAlchemy()
+from extensions import db
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
+    is_moderator = db.Column(db.Boolean, default=False)
 
     reviews = db.relationship('Review', backref='user', lazy=True)
     library = db.relationship('UserLibrary', backref='user', lazy=True)
@@ -15,7 +15,7 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     author = db.Column(db.String(50), nullable=False)
-    
+
     reviews = db.relationship('Review', backref='book', lazy=True)
 
 class Review(db.Model):
